01fa8c1d167c156968309f4fa62c7c06faa47cc1
Arthur <39200040+braga-arthur@users.noreply.github.com>
2019-05-29 12:45:19 -0300

    updates

---
+++ dbfake.js
@@ -0,0 +1,43 @@
+var db = {
+    bignoticias: [
+        {
+            titulo: "Bolsonaro mata a educação",
+            foto: 'https://uploads.metropoles.com/wp-content/uploads/2019/05/02185226/bolsonaro-anime-840x559.jpg',
+            descricao: "Assassinou a educação friamente assassinou a educação friamente Assassinou a educação friamente assassinou a educação friamente Assassinou a educação friamente assassinou a educação friamente",
+            autor: "Fulado da Maria Morereira",
+            data : "11/12/2017"
+        },
+        {
+            titulo: "Bolsonaro mata a gente de rir",
+            foto: 'https://catracalivre.com.br/wp-content/uploads/2019/03/bolsonaro-trump.jpg',
+            descricao: "mata a gente de rirmata a gente de rirmata a gente de rirmata a gente de rirmata a gente de rir hahahahahahahahahahahahahahahahahahahahhahaha",
+            autor: "Rita Maria da Silva",
+            data : "21/12/2012"
+        }
+    ],
+    normalnoticias: [
+        {
+            titulo: "Kpopers: ate quando?",
+            foto: 'http://www.digitalmusicnews.com/wp-content/uploads/2017/05/psy_BTS.jpg',
+            descricao: "Quem escuta KPOP nem eé genteQuem escuta KPOP nem eé genteQuem escuta KPOP nem eé gente",
+            autor: "Jose Carlos",
+            data : "02/05/2013"
+        },
+        {
+            titulo: "Usar crocs te deixa mais feio, diz qualquer pessoa com juizo",
+            foto: 'https://images-na.ssl-images-amazon.com/images/I/817YwNtQOML._UX395_.jpg',
+            descricao: "Crocs te deixa muito feio para de usar essa merdaCrocs te deixa muito feio para de usar essa merdaCrocs te deixa muito feio para de usar essa merda",
+            autor: "Jose Alencar",
+            data : "03/06/2013"
+        },
+        {
+            titulo: "Front end engineers, eu nao sou um mas estou aprendendo",
+            foto: 'https://process.filestackapi.com/cache=expiry:max/resize=width:1050/compress/o4EGqPvlQpLYUUI67a6g',
+            descricao: "Sera que ta bom? eu nao sei te responder essa pergunta meu jovem hahahahahahahahahahahahahahahahahahahahahahahahahaha",
+            autor: "Alencar Carlos",
+            data : "08/09/2013",
+            texto: "A front-end engineer, also known as a front-end web developer or designer, selects, installs and tests the user interface elements of a website. The front-end developer focuses on the look and feel of the site, along with its functionality. Job prospects remain strong for front-end engineers, particularly those who have the skills necessary to develop websites that are accessible on mobile devices. What Does a Front-End Engineer Do? Each front-end web designer job description differs according to employer need. However, front-end developers can usually expect to be part of a team that develops, renovates or maintains websites, either as an internal department within a company or as part of a third-party web solutions provider. Some developers are self-employed, working on a contract or on a freelance basis for clients."
+        }
+    ]
+
+}
